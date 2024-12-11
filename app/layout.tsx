14d257591d6cca1756e57f4cf512bd0298a23d82
dongyuexiang <dongyuexiang@bytedance.com>
2024-12-11 18:58:05 +0800

    feat: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,11 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-        <Script strategy="lazyOnload">{`<!-- TikTok Pixel Code Start -->
+        <Script strategy="lazyOnload">{``}</Script>
+<Script
+  id="tiktok-pixel-script"
+  dangerouslySetInnerHTML={{
+    __html: `<!-- TikTok Pixel Code Start -->
 <script>
 !function (w, d, t) {
   w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
@@ -21,7 +25,9 @@ var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n
   ttq.page();
 }(window, document, 'ttq');
 </script>
-<!-- TikTok Pixel Code End -->`}</Script>
+<!-- TikTok Pixel Code End -->`,
+  }}
+/>
       <body>{children}</body>
     </html>
   );
